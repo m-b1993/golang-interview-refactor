721911bbf4d77fa4a46c1fd043fa343d462c761c
Mehrdad <mehrdad.bahri@crowncapital.nz>
2024-02-06 18:09:10 +0330

    refactor: define custom type for cart status and use gorm enum type

--- pkg/entity/cart.go
+++ pkg/entity/cart.go
@@ -4,14 +4,16 @@ import (
 	"gorm.io/gorm"
 )
 
+type Status string
+
 const (
-	CartOpen   = "open"
-	CartClosed = "closed"
+	CartOpen   Status = "open"
+	CartClosed Status = "closed"
 )
 
 type CartEntity struct {
 	gorm.Model
 	Total     float64
 	SessionID string
-	Status    string
+	Status    Status `gorm:"type:enum('open', 'closed')"`
 }

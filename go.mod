5aa596cde5b42dffb47765d27d00d9cf7510eaad
Mehrdad <mehrdad.bahri@crowncapital.nz>
2024-02-06 11:58:33 +0330

    refactor: load applicaiton config

--- go.mod
+++ go.mod
@@ -4,13 +4,17 @@ go 1.21.0
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/google/uuid v1.6.0
+	github.com/qiangxue/go-env v1.0.1
 	go.uber.org/zap v1.26.0
+	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.5
 )
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/bytedance/sonic v1.10.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
---
+++ internal/config/config.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"interview/pkg/log"
+
+	"os"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/qiangxue/go-env"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	defaultServerPort = 8080
+)
+
+// Config represents an application configuration.
+type Config struct {
+	// the server port. Defaults to 8080
+	ServerPort int `yaml:"server_port" env:"SERVER_PORT"`
+	// the data source name (DSN) for connecting to the database. required.
+	DSN string `yaml:"dsn" env:"DSN,secret"`
+}
+
+// Validate validates the application configuration.
+func (c Config) Validate() error {
+	return validation.ValidateStruct(&c,
+		validation.Field(&c.DSN, validation.Required),
+	)
+}
+
+// Load returns an application configuration which is populated from the given configuration file and environment variables.
+func Load(file string, logger log.Logger) (*Config, error) {
+	// default config
+	c := Config{
+		ServerPort: defaultServerPort,
+	}
+
+	// load from YAML config file
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	if err = yaml.Unmarshal(bytes, &c); err != nil {
+		return nil, err
+	}
+
+	// load from environment variables prefixed with "INTERVIEW_"
+	if err = env.New("INTERVIEW_", logger.Infof).Load(&c); err != nil {
+		return nil, err
+	}
+
+	// validation
+	if err = c.Validate(); err != nil {
+		return nil, err
+	}
+
+	return &c, err
+}

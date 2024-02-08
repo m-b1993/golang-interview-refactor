7b96e8fe3c9ade501752a9aef9fea7541a7b2595
Mehrdad <mehrdad.bahri@crowncapital.nz>
2024-02-08 21:59:49 +0330

    refactor: extract getting user's cart to a function and reuse it

--- pkg/cart/service.go
+++ pkg/cart/service.go
@@ -5,20 +5,22 @@ import (
 	"errors"
 	"interview/pkg/entity"
 	"interview/pkg/log"
-
-	"gorm.io/gorm"
 )
 
 type Service interface {
 	AddItemToCart(ctx context.Context, product string, qty int) error
 	DeleteCartItem(ctx context.Context, cartItemID uint) error
 	GetCartItems(ctx context.Context) []map[string]interface{}
+	getCart(ctx context.Context) (entity.CartEntity, error)
+	getOrCreateCart(ctx context.Context) (entity.CartEntity, bool, error)
 }
 
 type service struct {
 	repo   Repository
 	logger log.Logger
 }
+
+var CartNotFoundError = errors.New("cart not found")
 
 func NewService(repo Repository, logger log.Logger) Service {
 	return service{repo, logger}
@@ -34,25 +36,19 @@ var itemPriceMapping = map[string]float64{
 }
 
 func (s service) GetCartItems(ctx context.Context) (items []map[string]interface{}) {
-	sessionID := ctx.Value("SessionId").(string)
-	conditions := map[string]interface{}{
-		"status":     entity.CartOpen,
-		"session_id": sessionID,
-	}
-	cartEntities, err := s.repo.QueryCart(ctx, conditions, "id desc", 1, 0)
-	if err != nil || len(cartEntities) == 0 {
+	cartEntity, err := s.getCart(ctx)
+	if err != nil {
+		s.logger.Errorf("error getting cart: %v", err)
 		return
 	}
-	cartEntity := cartEntities[0]
-
-	conditions = map[string]interface{}{
+	conditions := map[string]interface{}{
 		"cart_id": cartEntity.ID,
 	}
 	cartItems, err := s.repo.QueryCartItem(ctx, conditions, "id desc", 100, 0)
 	if err != nil {
+		s.logger.Errorf("error querying cart items: %v", err)
 		return
 	}
-
 	for _, cartItem := range cartItems {
 		item := map[string]interface{}{
 			"ID":       cartItem.ID,
@@ -60,75 +56,49 @@ func (s service) GetCartItems(ctx context.Context) (items []map[string]interface
 			"Price":    cartItem.Price,
 			"Product":  cartItem.ProductName,
 		}
-
 		items = append(items, item)
 	}
 	return items
 }
 
 func (s service) AddItemToCart(ctx context.Context, product string, qty int) error {
-	sessionID := ctx.Value("SessionId").(string)
-
-	var isCartNew bool
-	var cartEntity entity.CartEntity
-
-	conditions := map[string]interface{}{
-		"status":     entity.CartOpen,
-		"session_id": sessionID,
+	cartEntity, isCartNew, err := s.getOrCreateCart(ctx)
+	if err != nil {
+		return err
 	}
-	cartEntities, err := s.repo.QueryCart(ctx, conditions, "id desc", 1, 0)
-
-	if err != nil || len(cartEntities) == 0 {
-		if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
-			s.logger.Errorf("error querying cart: %v", err)
-			return errors.New("internal error")
-		}
-		isCartNew = true
-		cartEntity = entity.CartEntity{
-			SessionID: sessionID,
-			Status:    entity.CartOpen,
-		}
-		s.repo.CreateCart(ctx, &cartEntity)
-	} else {
-		cartEntity = cartEntities[0]
-	}
-
 	item, ok := itemPriceMapping[product]
 	if !ok {
 		return errors.New("invalid item name")
 	}
 	subTotal := item * float64(qty)
 
-	var cartItemEntity entity.CartItem
-	if isCartNew {
-		cartItemEntity = entity.CartItem{
+	createItem := func() {
+		cartItemEntity := entity.CartItem{
 			CartID:      cartEntity.ID,
 			ProductName: product,
 			Quantity:    qty,
 			Price:       subTotal,
 		}
 		s.repo.CreateCartItem(ctx, &cartItemEntity)
+	}
+
+	if isCartNew {
+		createItem()
 	} else {
-		conditions = map[string]interface{}{
+		conditions := map[string]interface{}{
 			"cart_id":      cartEntity.ID,
 			"product_name": product,
 		}
 		cartItems, err := s.repo.QueryCartItem(ctx, conditions, "id desc", 1, 0)
-		if err != nil || len(cartItems) == 0 {
-			if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
-				s.logger.Errorf("error querying cart item: %v", err)
-				return errors.New("internal error")
-			}
-			cartItemEntity = entity.CartItem{
-				CartID:      cartEntity.ID,
-				ProductName: product,
-				Quantity:    qty,
-				Price:       subTotal,
-			}
-			s.repo.CreateCartItem(ctx, &cartItemEntity)
+		if err != nil {
+			s.logger.Errorf("error querying cart item: %v", err)
+			return errors.New("internal error")
+		}
+		if len(cartItems) == 0 {
+			createItem()
 		} else {
-			cartItemEntity = cartItems[0]
-			cartItemEntity.Quantity += int(qty)
+			cartItemEntity := cartItems[0]
+			cartItemEntity.Quantity += qty
 			cartItemEntity.Price += subTotal
 			s.repo.UpdateCartItem(ctx, &cartItemEntity)
 		}
@@ -140,33 +110,23 @@ func (s service) AddItemToCart(ctx context.Context, product string, qty int) err
 }
 
 func (s service) DeleteCartItem(ctx context.Context, cartItemID uint) error {
-	sessionID := ctx.Value("SessionId").(string)
-
-	var cartEntity entity.CartEntity
-	conditions := map[string]interface{}{
-		"status":     entity.CartOpen,
-		"session_id": sessionID,
-	}
-	cartEntities, err := s.repo.QueryCart(ctx, conditions, "id desc", 1, 0)
-	if err != nil || len(cartEntities) == 0 {
-		s.logger.Errorf("error querying cart: %v", err)
+	cartEntity, err := s.getCart(ctx)
+	if err != nil {
+		s.logger.Errorf("error getting cart: %v", err)
 		return errors.New("internal error")
-	}
-	cartEntity = cartEntities[0]
-
-	if cartEntity.Status == entity.CartClosed {
-		return nil
 	}
 
 	var cartItemEntity entity.CartItem
-
-	conditions = map[string]interface{}{
+	conditions := map[string]interface{}{
 		"ID": cartItemID,
 	}
 	cartItems, err := s.repo.QueryCartItem(ctx, conditions, "id desc", 1, 0)
-	if err != nil || len(cartItems) == 0 {
+	if err != nil {
 		s.logger.Errorf("error querying cart item: %v", err)
 		return errors.New("internal error")
+	}
+	if len(cartItems) == 0 {
+		return errors.New("cart item not found")
 	}
 
 	cartItemEntity = cartItems[0]
@@ -177,4 +137,38 @@ func (s service) DeleteCartItem(ctx context.Context, cartItemID uint) error {
 	s.repo.DeleteCartItem(ctx, &cartItemEntity)
 
 	return nil
+}
+
+func (s service) getCart(ctx context.Context) (entity.CartEntity, error) {
+	sessionID := ctx.Value("SessionId").(string)
+	conditions := map[string]interface{}{
+		"status":     entity.CartOpen,
+		"session_id": sessionID,
+	}
+	cartEntities, err := s.repo.QueryCart(ctx, conditions, "id desc", 1, 0)
+	if err != nil {
+		return entity.CartEntity{}, err
+	}
+	if len(cartEntities) == 0 {
+		return entity.CartEntity{}, CartNotFoundError
+	}
+	return cartEntities[0], nil
+}
+
+func (s service) getOrCreateCart(ctx context.Context) (entity.CartEntity, bool, error) {
+	sessionID := ctx.Value("SessionId").(string)
+	created := false
+	cartEntity, err := s.getCart(ctx)
+	if err != nil && !errors.Is(err, CartNotFoundError) {
+		return entity.CartEntity{}, false, err
+	}
+	if errors.Is(err, CartNotFoundError) {
+		cartEntity = entity.CartEntity{
+			SessionID: sessionID,
+			Status:    entity.CartOpen,
+		}
+		s.repo.CreateCart(ctx, &cartEntity)
+		created = true
+	}
+	return cartEntity, created, nil
 }
